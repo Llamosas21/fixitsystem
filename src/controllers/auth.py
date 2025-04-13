6ca8200958c47426cd446733b0bcd93e6484cb00
Santiago Llamosas <nicolassantiagoll22@gmail.com>
2025-04-13 10:12:53 -0300

    Eliminadas las carpetas ignoradas del repositorio

--- src/controllers/auth.py
+++
@@ -1,6 +0,0 @@
-def verificar_usuario(usuario, contraseña):
-    usuarios_validos = {
-        "admin": "1234",
-        "tecnico": "abcd"
-    }
-    return usuarios_validos.get(usuario) == contraseña
