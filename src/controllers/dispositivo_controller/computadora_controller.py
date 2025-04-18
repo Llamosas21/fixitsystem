36faf5e945dc860f75fa1319dd9123178153b204
Santiago Llamosas <nicolassantiagoll22@gmail.com>
2025-04-18 14:52:38 -0300

    Añadido método editar_computadora al ComputadoraController para permitir actualizaciones en los datos de la computadora /n Se elimina redundancia en métodos y se optimiza la delegación de funciones al modelo

--- src/controllers/dispositivo_controller/computadora_controller.py
+++ src/controllers/dispositivo_controller/computadora_controller.py
@@ -8,9 +8,15 @@ class ComputadoraController:
         rows = self.modelo.obtener_computadoras()  # delegamos al modelo
         return rows 
 
+    def obtener_por_cliente_id(self, cliente_id):
+        return self.modelo.obtener_por_cliente_id(cliente_id)
+    
     def agregar_computadora(self, **kwargs):
         #print("🧩 Datos recibidos en agregar_computadora:", kwargs)
         self.modelo.agregar_computadora(**kwargs)
+
+    def editar_computadora(self, **kwargs):
+        self.modelo.editar_computadora(**kwargs)
 
     def cerrar_conexion(self):
         self.modelo.cerrar_conexion()
